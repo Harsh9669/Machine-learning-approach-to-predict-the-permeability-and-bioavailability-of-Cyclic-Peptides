c0cc76f1b41b078d27bd32c94dc7a25da80e5611
Harsh9669 <163961579+Harsh9669@users.noreply.github.com>
2024-08-10 21:01:08 +0530

    Update permeability2.py

--- permeability2.py
+++ permeability2.py
@@ -6,7 +6,7 @@ import numpy as np
 import pickle
 
 # Load the pre-trained model
-model_path = 'D:/C/trained_model2.sav'
+model_path = 'trained_model2.sav'
 with open(model_path, 'rb') as model_file:
     model = pickle.load(model_file)
 
